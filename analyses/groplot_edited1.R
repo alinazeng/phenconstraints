6a07a799c79161dd0df26e43779e9ea71a5b6c5b
AileneKane <aettinger@fasmail.harvard.edu>
2017-05-30 11:21:51 -0400

    looking at data from lizzie's experiment

--- analyses/groplot_edited1.R
+++ analyses/groplot_edited1.R
@@ -774,22 +774,61 @@ abline(lm(frstdoy~flowdur))
 
 
 #Look at Dan & Lizzie's data from growth chambers to see if it matches Sally's:
-d<-read.csv("data/leafoutdays_growthchamber.csv", header=T)
+#FIRST, WITH CHILLING
+d<-read.csv("data/leafoutdays_growthchamber.wlc1.csv", header=T)
+
 cols2 <- colorRampPalette(brewer.pal(8,"Accent"))(28)
 #First, later vs. earlier:
 quartz(height=3, width=7)
 par(mfrow=c(1,2))
-plot(d$BB.HF , d$LO.HF, ylab = "Leafout", xlab = "Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="Harvard Forest")
+plot(d$BB.WLC1.HF , d$LO.WLC1.HF, ylab = "Leafout", xlab = "Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="Harvard Forest")
+abline(lm(d$LO.WLC1.HF~d$BB.WLC1.HF))
+mtext(paste("r2=",round(summary((lm(d$LO.WLC1.HF~d$BB.WLC1.HF)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.WLC1.HF~d$BB.WLC1.HF))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+
+plot(d$BB.WLC1.SH , d$LO.WLC1.SH, ylab = "Leafout", xlab = "Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="St. Hippolyte")
+abline(lm(d$LO.WLC1.SH~d$BB.WLC1.SH))
+mtext(paste("r2=",round(summary((lm(d$LO.WLC1.SH~d$BB.WLC1.SH)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.WLC1.SH~d$BB.WLC1.SH))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+
+##Now , stages vs. interphenophase:
+d$LO_BB.WLC1.HF<-d$LO.WLC1.HF-d$BB.WLC1.HF
+d$LO_BB.WLC1.SH<-d$LO.WLC1.SH-d$BB.WLC1.SH
+
+quartz(height=6, width=7)
+par(mfrow=c(2,2))
+plot(d$LO_BB.WLC1.HF,d$BB.WLC1.HF,ylab = "Budburst", xlab = "Leafout-Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l",main="Harvard Forest- Chill only")
+mtext(paste("r2=",round(summary((lm(d$BB.WLC1.HF~d$LO_BB.WLC1.HF)))$r.squared, digits=2),", p=",round(summary(lm(d$BB.WLC1.HF~d$LO_BB.WLC1.HF))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+#abline(lm(d$BB.HF~d$LO_BB.HF))
+
+plot(d$LO_BB.WLC1.SH, d$BB.WLC1.SH,ylab = "Budburst", xlab = "Leafout-Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="St. Hippolyte- Chill only")
+mtext(paste("r2=",round(summary((lm(d$BB.WLC1.SH~d$LO_BB.WLC1.SH)))$r.squared, digits=2),", p=",round(summary(lm(d$BB.WLC1.SH~d$LO_BB.WLC1.SH))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+#abline(lm(d$BB.WLC1.SH~d$LO_BB.WLC1.SH))
+
+plot( d$LO_BB.WLC1.HF,d$LO.WLC1.HF,ylab = "Leafout", xlab = "Leafout-Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l")
+mtext(paste("r2=",round(summary((lm(d$LO.WLC1.HF~d$LO_BB.WLC1.HF)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.WLC1.HF~d$LO_BB.WLC1.HF))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+#abline(lm(d$LO.WLC1.HF~d$LO_BB.WLC1.HF))
+
+plot(d$LO_BB.WLC1.SH,d$LO.WLC1.SH, ylab = "Leafout", xlab = "Leafout-Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l")
+mtext(paste("r2=",round(summary((lm(d$LO.WLC1.SH~d$LO_BB.WLC1.SH)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.WLC1.SH~d$LO_BB.WLC1.SH))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
+#abline(lm(d$LO.WLC1.SH~d$LO_BB.WLC1.SH), lty=1, col="black")
+
+#NOW WITH ALL EXPERIMENTAL TREATMENTS:
+d<-read.csv("data/leafoutdays_growthchamber.csv", header=T)
+
+cols2 <- colorRampPalette(brewer.pal(8,"Accent"))(28)
+#First, later vs. earlier:
+quartz(height=3, width=7)
+par(mfrow=c(1,2))
+plot(d$BB.HF, d$LO.HF, ylab = "Leafout", xlab = "Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="Harvard Forest")
 abline(lm(d$LO.HF~d$BB.HF))
 mtext(paste("r2=",round(summary((lm(d$LO.HF~d$BB.HF)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.HF~d$BB.HF))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
 
 plot(d$BB.SH , d$LO.SH, ylab = "Leafout", xlab = "Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l", main="St. Hippolyte")
-abline(lm(d$LO.HF~d$BB.HF))
+abline(lm(d$LO.SH~d$BB.SH))
 mtext(paste("r2=",round(summary((lm(d$LO.SH~d$BB.SH)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.SH~d$BB.SH))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
 
 ##Now , stages vs. interphenophase:
 d$LO_BB.HF<-d$LO.HF-d$BB.HF
-d$LO_BB.SH<-d$LO.HF-d$BB.SH
+d$LO_BB.SH<-d$LO.SH-d$BB.SH
 
 quartz(height=6, width=7)
 par(mfrow=c(2,2))
@@ -808,4 +847,5 @@ mtext(paste("r2=",round(summary((lm(d$LO.HF~d$LO_BB.HF)))$r.squared, digits=2),"
 plot(d$LO_BB.SH,d$LO.SH, ylab = "Leafout", xlab = "Leafout-Budburst",pch=21,bg=cols2[as.numeric(as.factor(d$X))], bty="l")
 mtext(paste("r2=",round(summary((lm(d$LO.SH~d$LO_BB.SH)))$r.squared, digits=2),", p=",round(summary(lm(d$LO.SH~d$LO_BB.SH))$coeff[2,4],digits=3)), side=1, line=-1, cex=.6, adj=1)
 abline(lm(d$LO.SH~d$LO_BB.SH), lty=1, col="black")
+
 
