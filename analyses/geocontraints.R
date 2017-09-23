abc0c90facf1ee26d3cbe5ef70a3022ba58fcfe1
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2017-09-22 23:08:18 -0400

    Small tweaks to contraint models

--- analyses/geocontraints.R
+++ analyses/geocontraints.R
@@ -24,6 +24,11 @@ dat<-read.csv("phenomeans.csv", header=TRUE)
 # 6) Fru vs Flo
 # 7) Sen vs Fru
 
+# Build some fake data
+lo.fake <- rnorm(25, 60, 10)
+flo.fake <- lo.fake+20
+fr.fake <- lo.fake+120
+
 # test against slope of 1 ... 
 b = 1
 
@@ -52,6 +57,18 @@ plothyp1(dat$fr, dat$lo, "Fr vs LO")
 plothyp1(dat$fr, dat$fl, "Fr vs Fl")
 plothyp1(dat$sen, dat$fr, "Sen vs Fr")
 
+# the nonsig ones
+par(mfrow=c(3,1))
+plothyp1(dat$bb, dat$sen, "Sen vs BB")
+plothyp1(dat$lo, dat$sen, "Sen vs LO")
+plothyp1(dat$fl, dat$sen, "Sen vs Flo")
+
+
+# apply to fake data
+par(mfrow=c(2,1))
+plothyp1(lo.fake, flo.fake, "Fake: Flo vs LO")
+plothyp1(lo.fake, fr.fake, "Fake: Fru vs LO")
+
 
 ##
 ## Hypothesis 2 (interpheno predicts postphase)
@@ -64,20 +81,30 @@ plothyp1(dat$sen, dat$fr, "Sen vs Fr")
 
 # dive in and build a little f(x)
 plothyp2 <- function(postphase,interpheno,prephase, extraphase,name){
-    plot(postphase~interpheno, main=name)
-    for (i in 1:100){
+    plot(postphase~interpheno, main=name, type="n")
+    for (i in 1:999){
         interpheno.fake <- sample(interpheno, 25, replace=TRUE)
         postphase.fake <- prephase+interpheno.fake+extraphase
         abline(lm(postphase.fake~interpheno.fake), col="lightblue")
     }
-    abline(lm(postphase~interpheno), col="black", lwd=2)
+    #abline(lm(postphase~interpheno), col="black", lwd=2)
+    points(postphase~interpheno, main=name)
 }
 
 
 
 # now apply it
+# adj phases
 par(mfrow=c(2,2))
+plothyp2(dat$lo, dat$bb_lo, dat$bb, rep(0, 25), "LO vs BB_LO")
 plothyp2(dat$fl, dat$lo_fl, dat$lo, rep(0, 25), "Fl vs LO_Fl")
-plothyp2(dat$fr, dat$lo_fl, dat$lo, dat$fl, "Fr vs LO-Fl")
 plothyp2(dat$fr, dat$fl_fr, dat$fl, rep(0, 25),"Fr vs Fr-Fl")
-plothyp2(dat$sen, dat$fl_fr, dat$fr, rep(0, 25),"Sen vs Fr-Fl")
+plothyp2(dat$sen, dat$fr_sen, dat$fr, rep(0, 25), "Sen vs Fr_sen")
+
+par(mfrow=c(3,2)) # non-adj
+plothyp2(dat$fr, dat$lo_fl, dat$lo, rep(0, 25), "Fr vs LO-Fl")
+plothyp2(dat$sen, dat$fl_fr, dat$fl, rep(0, 25),"Sen vs Fr-Fl")
+plothyp2(dat$fl, dat$bb_lo, dat$bb, rep(0, 25), "Fl vs BB_LO")
+plothyp2(dat$fr, dat$bb_lo, dat$fl, rep(0, 25), "Fr vs BB_LO")
+plothyp2(dat$sen, dat$bb_lo, dat$bb, rep(0, 25), "Sen vs BB_LO") # tricky
+plothyp2(dat$sen, dat$lo_fl, dat$fl, rep(0, 25), "Sen vs LO_Fl") # also tricky
